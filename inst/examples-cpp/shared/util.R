1ab9163283a5187a532a30202347576756992a3d
Andrew Raim <andrew.raim@gmail.com>
2024-04-26 21:03:32 -0400

    Some re-organization

--- inst/examples-cpp/shared/util.R
+++ inst/examples-cpp/shared/util.R
@@ -5,12 +5,13 @@ printf = function(msg, ...)
 }
 
 # Mimic fprintf function in C
-fprintf = function(file, msg, ...) {
+fprintf = function(file, msg, ...)
+{
 	cat(sprintf(msg, ...), file = file)
 }
 
 # printf with timestamp
-logger =function (fmt, ..., dt_fmt = "%Y-%m-%d %H:%M:%S", join = " - ") 
+logger = function(fmt, ..., dt_fmt = "%Y-%m-%d %H:%M:%S", join = " - ")
 {
     sys.time = format(Sys.time(), format = dt_fmt)
     cat(sys.time, join, sprintf(fmt, ...), sep = "")
